dd54f73162388add8eb4e5985c1d0104a39ecd60
zhouwenfei <nick.zhou@hotmail.com>
2019-04-21 18:53:04 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/condition/MyImportSelector.java
@@ -0,0 +1,16 @@
+package com.atguigu.condition;
+
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+//自定义逻辑返回需要导入的组件
+public class MyImportSelector implements ImportSelector {
+	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+		//返回值，就是到导入到容器中的组件全类名
+		//AnnotationMetadata:当前标注@Import注解的类的所有注解信息
+		// TODO Auto-generated method stub
+		//importingClassMetadata
+		//方法不要返回null值
+		return new String[]{"com.atguigu.bean.Blue","com.atguigu.bean.Yellow"};
+	}
+}
